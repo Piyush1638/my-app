af668c937aa006ae250166cf56acee66db380f22
Piyush Kumar Singh <21bcs1638@cuchd.in>
2024-01-26 21:28:35 +0530

    Hulk Cars

---
+++ components/HeroCarousel.tsx
@@ -0,0 +1,57 @@
+"use client"
+import * as React from "react"
+import Card from "@/components/Card"
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export function HeroCarousel() {
+  const [api, setApi] = React.useState<CarouselApi>()
+  const [current, setCurrent] = React.useState(0);
+
+  const [count, setCount] = React.useState(0)
+ 
+  React.useEffect(() => {
+    if (!api) {
+      return
+    }
+ 
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap() + 2)
+ 
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 2)
+    })
+  }, [api])
+
+
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      setApi={setApi}
+      className="w-full"
+    >
+      <CarouselContent>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/5">
+            <div className={`${current === index ? "scale-125":"scale-100"}`}>
+              <Card/>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
+
+// export default HeroCarousel
