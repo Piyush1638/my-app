9f267f73c1680ec6dd4354ce0b3d928d0a5b8f27
Piyush Kumar Singh <21bcs1638@cuchd.in>
2024-01-29 15:23:12 +0530

    Added FAQ Page

---
+++ app/faq/page.tsx
@@ -0,0 +1,15 @@
+import AccordionDiv from "@/components/faq/AccordionDiv";
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="min-h-screen overflow-x-hidden mf:px-10 mf:pt-32 px-3 pt-4 pb-0 bg-primary">
+      <h1 className="text-slate-50 text-3xl font-bold text-center">FAQ</h1>
+      <div className="flex items-center justify-center">
+        <AccordionDiv />
+      </div>
+    </section>
+  );
+};
+
+export default page;
