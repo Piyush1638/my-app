0bf868b9567aaf02b6cfb0f45e856472b05872c2
lalman888 <chauhanlakshay074@gmail.com>
2024-01-31 02:13:17 +0530

    color fix

--- components/dashboard/Income.tsx
+++ components/dashboard/Income.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DailySaleSvg from "./DailySaleSvg";
 
 const Income = () => {
   return (
@@ -33,21 +34,22 @@ const Income = () => {
         </div>
       </div>
       <div className="flex items-stretch justify-between gap-1.5 mt-12 max-md:max-w-full max-md:flex-wrap max-md:mr-1 max-md:mt-10">
-        <div className="flex basis-0 flex-col items-stretch text-sm text-white whitespace-nowrap text-right">
+        {/* <div className="flex basis-0 flex-col items-stretch text-sm text-white whitespace-nowrap text-right">
           <div className="leading-[140%] dark:text-white text-black">600</div>
           <div className="leading-[140%] mt-6 dark:text-white text-black">400</div>
           <div className="leading-[140%] mt-6 dark:text-white text-black">200</div>
           <div className="leading-[140%] mt-6 dark:text-white text-black">0</div>
-        </div>
+        </div> */}
         <div className="">
-          <img
+          {/* <img
             loading="lazy"
             srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/cb392a1ccf73a8a4ca79de435f85178a3c6fb7b088897ee4d4f556518aed7371?apiKey=386f6655ee2e4fd59eb38f1897c7f7b2&"
             className="aspect-[8.33] object-cover object-center w-[787px] my-auto max-md:max-w-full"
-          />
+          /> */}
+          <DailySaleSvg />
         </div>
       </div>
-      <div className="self-center flex w-full max-w-[735px] items-stretch justify-between gap-5 text-sm text-white whitespace-nowrap text-center mt-4 max-md:max-w-full max-md:flex-wrap">
+      {/* <div className="self-center flex w-full max-w-[735px] items-stretch justify-between gap-5 text-sm text-white whitespace-nowrap text-center mt-4 max-md:max-w-full max-md:flex-wrap">
         <div className="leading-[140%] grow dark:text-white text-black">Apr</div>
         <div className="leading-[140%] dark:text-white text-black">May</div>
         <div className="leading-[140%] dark:text-white text-black">Jun</div>
@@ -57,7 +59,7 @@ const Income = () => {
         <div className="leading-[140%] dark:text-white text-black">Oct</div>
         <div className="leading-[140%] dark:text-white text-black">Nov</div>
         <div className="leading-[140%] grow dark:text-white text-black">Dec</div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- components/dashboard/RentedCar.tsx
+++ components/dashboard/RentedCar.tsx
@@ -1,9 +1,10 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
 const RentedCar = () => {
   return (
-    <div className="items-stretch self-stretch darK:bg-[#1D1F25] flex w-auto relative flex-col p-5 rounded-xl bg-[#FFF] border border-[#F1F1F1]">
+    <div className="items-stretch self-stretch dark:bg-[#1D1F25] dark:border-[#1D1F25] flex w-auto relative flex-col p-5 rounded-xl bg-[#FFF] border border-[#F1F1F1]">
       <header className="justify-between items-stretch flex w-full gap-5">
         <div className="justify-between items-stretch flex gap-1 my-2">
           <div className="dark:text-white text-[#10141A] text-base font-bold leading-6 grow whitespace-nowrap">
@@ -12,9 +13,13 @@ const RentedCar = () => {
           <div className="flex w-3 shrink-0 h-3 bg-green-400 ml-1 my-auto rounded-full" />
         </div>
         <div className="items-stretch flex gap-2">
-          <Services href="#" src="/assets/dashboard/gas-station.png" />
+          <Services href="#" src="/assets/dashboard/gas-station.png" 
+          active={true}
+          />
           <Services href="#" src="/assets/dashboard/Vector.png" />
-          <Services href="#" src="/assets/dashboard/Vector-1.png" />
+          <Services href="#" src="/assets/dashboard/Vector-1.png" 
+          
+          />
         </div>
       </header>
       <div className="w-full">
@@ -33,10 +38,13 @@ const RentedCar = () => {
 
 export default RentedCar;
 
-const Services = ({ href, src }: { href: string; src: string }) => (
+const Services = ({ href, src,active }: { href: string; src: string;active?:boolean }) => (
   <a
     href={href}
-    className="justify-center items-center bg-gray-800 flex aspect-square flex-col w-[30px] h-[30px] px-2 rounded-[55px]"
+    className={cn(
+      "justify-center bg-black/40 items-center  flex aspect-square flex-col w-[30px] h-[30px] px-2 rounded-[55px]",
+      active && "bg-gray-800"
+    )}
     aria-label="Car Image 1"
     role="img"
   >
